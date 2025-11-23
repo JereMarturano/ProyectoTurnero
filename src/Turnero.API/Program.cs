7cd10d21ae836fe02114af623632aca99504aadb
JereMarturano <Jeremiasmarturano1@gmail.com>
2025-11-23 19:26:43 -0300

    feat: initialize ASP.NET Core API project with database, JWT authentication, SignalR, and Docker for SQL Server.

--- src/Turnero.API/Program.cs
+++ src/Turnero.API/Program.cs
@@ -10,7 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddDbContext<TurneroDbContext>(options =>
-    options.UseInMemoryDatabase("TurneroDb"));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddScoped<AuthService>();
 builder.Services.AddScoped<TurnService>();
